97e48b20b236e3953c128189d0777096cd28d91d
Suhaib Chaudhry <suhaib@uitoux.com>
2015-04-23 01:36:16 -0400

    Paralox scrolling modifications (Asad)

--- sites/pinnacle.com/themes/pinnacle/js/pinnacle_custom.js
+++ sites/pinnacle.com/themes/pinnacle/js/pinnacle_custom.js
@@ -18,6 +18,8 @@
 					that.$window.unbind("load resize scroll", callback);
 					that.$zoneWrap.css({backgroundPosition: 'center top'});	
 				}
+
+				that.getBgDimensions(that.$zoneWrap);
 			});
 
 			this.firstLoad = false;
@@ -36,6 +38,49 @@
 		return function() {			
 			method.apply(context, arguments);
 		}
-	}
+	},
+	//Source: http://jsfiddle.net/TLQrL/2/
+	//Stackoverflow: Question: http://stackoverflow.com/questions/23518501/retrieve-the-size-of-a-background-image-set-to-cover
+  	getBgDimensions: function(zone) {
+  		var that = this;
+ 		var div = zone.get(0);
+		var style = div.currentStyle || window.getComputedStyle(div, false);
+		var bg = style.backgroundImage.slice(4, -1);
+
+		var background = new Image();
+		background.src = bg;
+
+		background.onload = function() {
+		    if (background.width > background.height) {
+		        var ratio = background.height / background.width;
+		        if (div.offsetWidth > div.offsetHeight) {
+		            var bgW = div.offsetWidth;
+		            var bgH = Math.round(div.offsetWidth * ratio);
+		            if (bgH < div.offsetHeight) {
+		                bgH = div.offsetHeight;
+		                bgW = Math.round(bgH / ratio);
+		            }
+		        } else {
+		            var bgW = Math.round(div.offsetHeight / ratio);
+		            var bgH = div.offsetHeight;
+		        }
+		    } else {
+		        var ratio = background.width / background.height;
+		        if (div.offsetHeight > div.offsetWidth) {
+		            var bgH = div.offsetHeight;
+		            var bgW = Math.round(div.offsetHeight * ratio);
+		            if (bgW > div.offsetWidth) {
+		                bgW = div.offsetWidth;
+		                bgH = Math.round(bgW / ratio);
+		            }
+		        } else {
+		            var bgW = Math.round(div.offsetWidth / ratio);
+		            var bgH = div.offsetWidth;
+		        }
+		    }
+		    console.log(bgW + ", " + bgH);
+		    that.max_image_position = bgH-zone.height();
+		}
+  	}
   }
 })(jQuery);
